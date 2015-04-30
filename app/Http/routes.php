d0bc449ab9b9307a6f428b7a86d5231cbfb068fb
Cristiano Pacheco <chris.spb25@gmail.com>
2015-04-30 13:06:49 -0300

    Capitulo 3-Rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,15 +11,67 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::pattern('category','[0-9]+');
+Route::pattern('product','[0-9]+');
+Route::pattern('id','[0-9]+');
+Route::group(['prefix'=>'admin'], function (){
 
-Route::get('home', 'HomeController@index');
+    // Rotas de Category
+    Route::get('categories', [
+        'as' => 'categories',
+        'uses' => 'AdminCategoriesController@index'
+    ]);
 
-Route::get('exemplo', 'WelcomeController@exemplo');
+    Route::get('categories/{category}', [
+        'as' => 'editCategory', function (\CodeCommerce\Category $category){
+            echo $category->name;
+        }
+    ]);
 
-Route::get('admin/products', 'AdminProductsController@index');
+    Route::put('categories/{id}', [
+        'as' => 'saveEditCategory',
+        'uses' => 'AdminCategoriesController@save'
+    ]);
 
-Route::get('admin/categories', 'AdminCategoriesController@index');
+    Route::post('categories', [
+        'as' => 'saveCategory',
+        'uses' => 'AdminCategoriesController@save'
+    ]);
+
+    // Rotas de Products
+    Route::get('products', [
+        'as' => 'products',
+        'uses' => 'AdminProductsController@index'
+    ]);
+
+    Route::get('products/{product}', [
+        'as' => 'editProduct', function (\CodeCommerce\Product $product){
+            echo $product->name;
+        }
+    ]);
+
+    Route::put('products/{id}', [
+        'as' => 'saveEditProduct',
+        'uses' => 'AdminProductsController@save'
+    ]);
+
+    Route::post('products', [
+        'as' => 'saveProduct',
+        'uses' => 'AdminProductsController@save'
+    ]);
+
+
+});
+
+// Route::get('/', 'WelcomeController@index');
+
+// Route::get('home', 'HomeController@index');
+
+// Route::get('exemplo', 'WelcomeController@exemplo');
+
+// Route::get('admin/products', 'AdminProductsController@index');
+
+// Route::get('admin/categories', 'AdminCategoriesController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
